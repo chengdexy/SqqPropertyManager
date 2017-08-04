1cee54b835c2ce62d57bfdd1f536f6808cffe31f
Xue <chengdexy@sina.com>
2017-08-04 16:12:15 +0800

    添加项目文件。

---
+++ SqqPropertyManager/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SqqPropertyManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(76, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(76, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(182, 23);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(278, 6);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(90, 40);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "读取";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Enabled = false;
+            this.btnAdd.Location = new System.Drawing.Point(28, 91);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 40);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Enabled = false;
+            this.btnDel.Location = new System.Drawing.Point(124, 91);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(90, 40);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "删除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "单位:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "模块:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 151);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "双桥区网站权限管理器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SqqPropertyManager/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SqqPropertyManager
+{
+    public partial class Form1 : Form
+    {
+        public static SqlConnection cnn = new SqlConnection("Data Source=bds-001.hichina.com;Initial Catalog=bds0010431_db;User Id=bds0010431;Password=sfvhj2569; ");
+        public static SqlCommand cmd;
+
+        public static int curDeptID = 0;
+        public static int curFuncID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dictionary<int, string> dicDept = new Dictionary<int, string>();
+            cnn.Open();
+            string sql = "select * from dept";
+            cmd = new SqlCommand(sql, cnn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                dicDept.Add(Convert.ToInt32(reader["id"]), reader["deptname"].ToString());
+            }
+            BindingSource bsDept = new BindingSource();
+            bsDept.DataSource = dicDept;
+            comboBox1.DataSource = bsDept;
+            comboBox1.DisplayMember = "value";
+            comboBox1.ValueMember = "key";
+            reader.Close();
+
+            int[] parentIds;
+            sql = "select distinct parentid from functions";
+            cmd.CommandText = sql;
+            reader = cmd.ExecuteReader();
+            List<int> parentList = new List<int>();
+            while (reader.Read())
+            {
+                parentList.Add(Convert.ToInt32(reader["parentid"]));
+            }
+            parentIds = parentList.ToArray<int>();
+            reader.Close();
+
+            Dictionary<int, string> dicFunc = new Dictionary<int, string>();
+            sql = "select * from functions";
+            cmd = new SqlCommand(sql, cnn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (!parentIds.Contains(Convert.ToInt32(reader["id"])))
+                {
+                    dicFunc.Add(Convert.ToInt32(reader["id"]), reader["funname"].ToString());
+                }
+            }
+            BindingSource bsFunc = new BindingSource();
+            bsFunc.DataSource = dicFunc;
+            comboBox2.DataSource = bsFunc;
+            comboBox2.DisplayMember = "value";
+            comboBox2.ValueMember = "key";
+
+            reader.Close();
+            cmd.Dispose();
+            cnn.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = false;
+            btnDel.Enabled = false;
+
+            try
+            {
+                curDeptID = Convert.ToInt32(((KeyValuePair<int, string>)comboBox1.SelectedValue).Key);
+            }
+            catch
+            {
+                curDeptID = Convert.ToInt32(comboBox1.SelectedValue);
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = false;
+            btnDel.Enabled = false;
+
+            try
+            {
+                curFuncID = Convert.ToInt32(((KeyValuePair<int, string>)comboBox2.SelectedValue).Key);
+            }
+            catch
+            {
+                curFuncID = Convert.ToInt32(comboBox2.SelectedValue);
+            }
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (curDeptID != 0 && curFuncID != 0)
+            {
+                cnn.Open();
+                string sql = $"select funids from dept where id={curDeptID}";
+                cmd = new SqlCommand(sql, cnn);
+                string funids = cmd.ExecuteScalar().ToString();
+                string[] ids = funids.Split(',');
+                if (ids.Contains<string>(curFuncID.ToString()))
+                {
+                    btnAdd.Enabled = false;
+                    btnDel.Enabled = true;
+                }
+                else
+                {
+                    btnAdd.Enabled = true;
+                    btnDel.Enabled = false;
+                }
+                cnn.Close();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            AddNewFunc(curFuncID);
+            cnn.Close();
+            MessageBox.Show($"已添加权限:{curFuncID }");
+        }
+
+        private static void AddNewFunc(int id)
+        {
+            //先检查此节点是否有父节点
+            //如果有父节点,且父节点的父节点不为0,则先加入其父节点
+
+
+            string sql2 = $"select * from functions where id={id}";
+            cmd.CommandText = sql2;
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                int parentid = Convert.ToInt32(reader["parentid"]);
+                reader.Close();
+                if (parentid != 0)
+                {
+                    AddNewFunc(parentid);
+                }
+
+            }
+
+            string sql = $"select funids from dept where id={curDeptID}";
+            cmd.CommandText = sql;
+            string[] ids = cmd.ExecuteScalar().ToString().Split(',');
+            List<string> lst = ids.ToList<string>();
+            lst.Add(id.ToString());
+            ids = lst.ToArray<string>();
+            string finalString = string.Join(",", ids);
+
+            sql = $"update dept set funids='{finalString }' where id={curDeptID}";
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            cnn.Open();
+            DelFun(curFuncID);
+            cnn.Close();
+            MessageBox.Show($"已删除权限:{curFuncID }");
+        }
+
+        private static void DelFun(int id)
+        {
+            //查看其父id
+            int parentid = checkParent(id);
+            //删除此项
+            string sql = $"select funids from dept where id={curDeptID}";
+            cmd.CommandText = sql;
+            string[] ids = cmd.ExecuteScalar().ToString().Split(',');
+            if (ids.Contains<string>(id.ToString()))
+            {
+                ids = ids.Where(x => x != id.ToString()).ToArray();
+            }
+            string finalString = string.Join(",", ids);
+
+            sql = $"update dept set funids='{finalString }' where id={curDeptID}";
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            //检差是否还有同父的项
+            bool hasBrother = false;
+            if (parentid != 0)
+            {
+                foreach (string item in ids)
+                {
+                    int theID = Convert.ToInt32(item);
+                    int theParentID = checkParent(theID);
+                    if (theParentID == parentid)
+                    {
+                        //有
+                        hasBrother = true;
+                    }
+                }
+            }
+            if (!hasBrother && parentid != 0)
+            {
+                //如果没兄弟节点
+                DelFun(parentid);
+            }
+        }
+
+        private static int checkParent(int id)
+        {
+            string sql2 = $"select parentid from functions where id={id}";
+            cmd.CommandText = sql2;
+            int parentid = Convert.ToInt32(cmd.ExecuteScalar());
+            return parentid;
+        }
+    }
+}
